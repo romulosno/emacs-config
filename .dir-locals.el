19d3640f59d25537f0a4fabec30b39fa3239ee9c
romulosno <romulosnoliveira@gmail.com>
2024-06-23 20:35:26 -0300

    Add .dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode . ((outline-regexp . "\\;;;\\|;;;;\\|;; Local")
+		     (lexical-binding . t))))
