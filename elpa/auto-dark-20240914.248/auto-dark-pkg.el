caaf240bc6e811385a4b8a194f622b29a13f86d7
romulosno <romulosnoliveira@gmail.com>
2024-09-15 13:50:55 -0300

    Add packages

---
+++ elpa/auto-dark-20240914.248/auto-dark-pkg.el
@@ -0,0 +1,18 @@
+(define-package "auto-dark" "20240914.248" "Automatically sets the dark-mode theme based on macOS/Linux/Windows status"
+  '((emacs "24.4"))
+  :commit "681537d9ae770a51fc12329e7c70e4465506d5ec" :authors
+  '(("Tim Harper" . "timcharperatgmaildotcom")
+    ("Vincent Zhang" . "seagle0128@gmail.com")
+    ("Jonathan Arnett" . "jonathan.arnett@protonmail.com"))
+  :maintainers
+  '(("Tim Harper" . "timcharperatgmaildotcom")
+    ("Vincent Zhang" . "seagle0128@gmail.com")
+    ("Jonathan Arnett" . "jonathan.arnett@protonmail.com"))
+  :maintainer
+  '("Tim Harper" . "timcharperatgmaildotcom")
+  :keywords
+  '("macos" "windows" "linux" "themes" "tools" "faces")
+  :url "https://github.com/LionyxML/auto-dark-emacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
