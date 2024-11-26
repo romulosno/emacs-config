25e8d6c053350d4349f542c2c71ee5ed35040102
Romulo Souza Nascimento Oliveira <romulo@doa>
2024-11-25 22:35:54 -0300

    Remove init items

--- early-init.el
+++ early-init.el
@@ -8,3 +8,8 @@
 (tooltip-mode -1)
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Remove init items
+(advice-add #'display-startup-echo-area-message :override #'ignore)
+(advice-add #'display-startup-screen :override #'ignore)
+
