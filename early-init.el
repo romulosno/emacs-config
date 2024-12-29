1f8f9c49a26b8765cc7fcd59a4545f39a20b802e
Romulo Oliveira <romulosnoliveira@gmail.com>
2024-12-29 18:32:06 -0300

    Update

---
+++ early-init.el
@@ -0,0 +1,9 @@
+(setq gc-cons-threshold 51200000)
+(setq gc-cons-percentage 0.4)
+
+;;; UI
+(blink-cursor-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
--- init.el
+++ init.el
@@ -1,16 +1,4 @@
 ;;; init.el --- emacs config -*- lexical-binding: t; -*-
-(setq gc-cons-threshold 51200000)
-(setq gc-cons-percentage 0.4)
-
-;;; UI
-(blink-cursor-mode -1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tooltip-mode -1)
-
-(add-to-list 'default-frame-alist '(font . "Fira Code-10"))
-
 (setopt custom-theme-directory (locate-user-emacs-file "themes"))
 (load-theme 'cores-claras t)
 (load-theme 'cores-escuras t t)
