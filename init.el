cb41ec089532608bfca505770f6b45021e09347e
Romulo Oliveira <romulosnoliveira@gmail.com>
2025-01-05 22:16:52 -0300

    Update

--- init.el
+++ init.el
@@ -1,5 +1,4 @@
-;;; init.el --- emacs config -*- lexical-binding: t; -*-
-(load-theme 'cores-claras t)
+;;; init.el --- emacs config -*- lexical-binding: t; eval: (outline-minor-mode 1); -*-
 
 ;;; Package
 (unless package-archives
@@ -11,13 +10,16 @@
 	 go-mode
 	 kotlin-mode
 	 yaml-mode
-	 markdown-mode)))
+	 markdown-mode
+	 modus-themes)))
   (dolist (p package-list)
     (unless (package-installed-p p)
       (package-install p))))
 
+;;; Theme and font
+(load-theme 'modus-operandi-tinted t)
+
 ;;; Backups
-(setq vc-make-backup-files t)
 (setq backup-by-copying t)
 (setq delete-old-versions t)
 (setq version-control t)
@@ -30,13 +32,12 @@
 (setq tramp-auto-save-directory (locate-user-emacs-file "tramp-autosave/"))
 
 ;;; Init screen
-(setq initial-major-mode 'fundamental-mode
-      inhibit-startup-screen t
-      inhibit-splash-screen t
-      initial-scratch-message nil)
+(setq initial-major-mode 'fundamental-mode)
+(setq inhibit-startup-screen t)
+(setq inhibit-splash-screen t)
+(setq initial-scratch-message nil)
 
 ;;; Misc
-(setq-default truncate-lines t)
 (setq use-short-answers t)
 (setq enable-recursive-minibuffers t)
 (setq kill-whole-line t)
@@ -46,38 +47,43 @@
 (setq read-minibuffer-restore-windows nil)
 (setq sentence-end-double-space nil)
 (setq uniquify-buffer-name-style 'forward)
-(setq repeat-exit-key "RET")
 (setq find-file-visit-truename t)	
 
-;; Scroll
-(setq fast-but-imprecise-scrolling t)
-(setq jit-lock-defer-time 0)
-
-;; Enable commands
-(put 'dired-find-alternate-file 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
 
+;;; Keybindings remap
+(global-set-key [remap count-words-region] #'count-words)
+(global-set-key [remap capitalize-word] #'capitalize-dwim)
+(global-set-key [remap downcase-word] #'downcase-dwim)
+(global-set-key [remap upcase-word] #'upcase-dwim)
+(global-set-key [remap list-buffers] #'electric-buffer-list)
+
+;; Other keys
+(global-set-key (kbd "C-c k") #'kill-current-buffer)
+(global-set-key (kbd "M-o") #'other-window)
+(global-set-key (kbd "M-O") #'other-frame)
+(global-set-key (kbd "C-z") #'repeat)
+
 ;;; Smart parens
-(setq show-paren-when-point-inside-paren t
-      show-paren-when-point-in-periphery t)
+(setq show-paren-when-point-inside-paren t)
+(setq show-paren-when-point-in-periphery t)
 
 ;;; Isearch
-(setq isearch-lazy-count t
-      isearch-yank-on-move 'shift
-      isearch-allow-scroll t)
+(setq isearch-lazy-count t)
+(setq isearch-yank-on-move 'shift)
+(setq isearch-allow-scroll t)
+(setq isearch-lazy-highlight 'all-windows)
 
 ;;; Completions
-(setq read-buffer-completion-ignore-case t
-      read-file-name-completion-ignore-case t)
+(setq read-buffer-completion-ignore-case t)
+(setq read-file-name-completion-ignore-case t)
 
-(setq tab-always-indent 'complete
-      completion-show-help nil
-      completions-header-format nil
-      completions-format 'one-column
-      completions-max-height 20)
+(setq tab-always-indent 'complete)
+(setq completion-show-help nil)
+(setq completions-header-format nil)
+(setq completion-styles '(basic partial-completion emacs22 substring initials))
 
-(setq completion-styles
-      '(basic partial-completion emacs22 substring flex))
+(define-key completion-in-region-mode-map (kbd "M-v") #'switch-to-completions)
 
 ;;; Abbrev
 (setq-default abbrev-mode t)
@@ -86,17 +92,22 @@
 (setq abbrev-file-name (expand-file-name "abbrev_defs" user-emacs-directory))
 
 ;;; Dired
-(require 'dired)
 (require 'dired-aux)
 (require 'dired-x)
+(require 'ls-lisp)
 
-(setq delete-by-moving-to-trash t
-      dired-kill-when-opening-new-dired-buffer t
-      dired-recursive-copies 'always
-      dired-recursive-deletes 'always)
+(setq ls-lisp-dirs-first t)
+(setq ls-lisp-use-insert-directory-program nil)
+
+(setq delete-by-moving-to-trash t)
+(setq dired-kill-when-opening-new-dired-buffer t)
+(setq dired-recursive-copies 'always)
+(setq dired-recursive-deletes 'always)
 
 (add-hook 'dired-mode-hook #'dired-hide-details-mode)
 (add-hook 'dired-mode-hook #'hl-line-mode)
+
+(global-set-key (kbd "C-c f") #'find-name-dired)
 
 ;;; Outline
 (setq outline-minor-mode-prefix "\C-co")
@@ -106,39 +117,48 @@
 (put 'outline-default-state 'safe-local-variable #'integerp)
 (put 'outline-default-state 'safe-local-variable #'symbolp)
 
+(global-set-key (kbd "<f6>") #'outline-minor-mode)
+
 (add-hook 'outline-minor-mode-hook
 	  (lambda ()
-	    (if outline-minor-mode
-		(reveal-mode 1)
-	      (reveal-mode -1))))
+	    (if (null outline-minor-mode)
+		(reveal-mode -1)
+	      (outline-hide-sublevels 100)
+	      (reveal-mode 1))))
+
+
 
 ;;; Org
-(add-hook 'org-mode-hook #'auto-fill-mode)
+(setq org-tags-column 0)
+(setq org-use-speed-commands t)
+(setq org-export-with-sub-superscripts nil)
+(setq org-return-follows-link t)
+(setq org-startup-folded nil)
+(setq org-startup-indented t)
+(setq org-agenda-show-log t)
 
-(setq org-tags-column 0
-      org-use-speed-commands t
-      org-export-with-sub-superscripts nil
-      org-return-follows-link t
-      org-startup-folded nil
-      org-startup-indented t)
+(setq org-log-done 'time)
+(setq org-log-into-drawer t)
 
-(setq org-agenda-show-log t
-      org-log-done 'time
-      org-log-into-drawer t)
-
-(setq org-goto-interface 'outline-path-completion
-      org-goto-max-level 10)
+(setq org-goto-interface 'outline-path-completion)
+(setq org-goto-max-level 10)
 
 (put 'org-todo-keyword-faces 'safe-local-variable #'stringp)
 
-;;; Version Control
-(setq vc-git-show-stash 0
-      vc-follow-symlinks t
-      vc-git-print-log-follow t
-      vc-handled-backends '(Git))
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c l") #'org-store-link)
+(with-eval-after-load 'org
+  (define-key org-mode-map (kbd "C-M-<return>") #'org-insert-subheading))
 
-(setq ediff-window-setup-function 'ediff-setup-windows-plain
-      ediff-keep-variants nil)
+
+;;; Version Control
+(setq vc-git-show-stash 0)
+(setq vc-follow-symlinks t)
+(setq vc-git-print-log-follow t)
+(setq vc-handled-backends '(Git))
+
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+(setq ediff-keep-variants nil)
 
 (setq smerge-command-prefix "\e")
 (setq diff-default-read-only t)
@@ -152,81 +172,47 @@
 
 (add-hook 'compilation-filter-hook #'ansi-color-compilation-filter)
 
-(setq compilation-max-output-line-length nil
-      compilation-scroll-output 'first-error
-      compilation-auto-jump-to-first-error 'if-location-known
-      compilation-ask-about-save nil)
-
-(setopt project-compilation-buffer-name-function
-        (lambda (name-of-mode)
-          (generate-new-buffer-name
-           (project-prefixed-buffer-name name-of-mode))))
-
-(setq eldoc-documentation-strategy 'eldoc-documentation-compose-eagerly
-      eldoc-echo-area-use-multiline-p 3)
-
-(setq eglot-autoshutdown t
-      eglot-sync-connect nil
-      eglot-events-buffer-config '(:size 500000))
-
-(setq read-process-output-max (* 3 1024 1024))
-
-;;; Global modes
-(column-number-mode 1)
-(save-place-mode 1)
-(auto-save-visited-mode 1)
-(global-so-long-mode 1)
-(winner-mode 1)
-(minibuffer-depth-indicate-mode 1)
-(recentf-mode 1)
-(savehist-mode 1)
-(repeat-mode 1)
-
-;;; Keybindings
-(global-set-key [remap count-words-region] #'count-words)
-(global-set-key [remap capitalize-word] #'capitalize-dwim)
-(global-set-key [remap downcase-word] #'downcase-dwim)
-(global-set-key [remap upcase-word] #'upcase-dwim)
-;; (global-set-key [remap list-buffers] #'electric-buffer-list)
-
-;;;; Ctrl-c prefix
-(global-set-key (kbd "C-c f") #'find-name-dired)
-(global-set-key (kbd "C-c k") #'kill-current-buffer)
-(global-set-key (kbd "C-c r") #'recentf)
-(global-set-key (kbd "C-c a") #'org-agenda)
-(global-set-key (kbd "C-c l") #'org-store-link)
-
-;;;; Misc
-(global-set-key (kbd "M-o") #'other-window)
-(global-set-key (kbd "M-O") #'other-frame)
-(global-set-key (kbd "C-z") #'repeat)
-(global-set-key (kbd "<f6>") #'outline-minor-mode)
 (global-set-key (kbd "<f5>") #'compile)
 
-;;;; Completions
-(define-key completion-in-region-mode-map (kbd "M-v") #'switch-to-completions)
+(setq compilation-max-output-line-length nil)
+(setq compilation-scroll-output 'first-error)
+(setq compilation-auto-jump-to-first-error 'if-location-known)
+(setq compilation-ask-about-save nil)
 
-;;;; Eglot
+(setq eldoc-documentation-strategy 'eldoc-documentation-compose-eagerly)
+(setq eldoc-echo-area-use-multiline-p 3)
+
+;;; Eglot
+(setq eglot-autoshutdown t)
+(setq eglot-sync-connect nil)
+(setq eglot-events-buffer-config '(:size 500000))
+(setq read-process-output-max (* 3 1024 1024))
+
 (with-eval-after-load 'eglot
   (define-key eglot-mode-map (kbd "C-c c r") #'eglot-rename)
   (define-key eglot-mode-map (kbd "C-c c f") #'eglot-format)
   (define-key eglot-mode-map (kbd "C-c c a") #'eglot-code-actions))
 
-;;;; Flymake
+;;; Flymake
 (with-eval-after-load 'flymake
   (define-key flymake-mode-map (kbd "C-c e") #'flymake-show-project-diagnostics)
   (define-key flymake-mode-map (kbd "<f7>") #'flymake-goto-next-error)
   (define-key flymake-mode-map (kbd "<f8>") #'flymake-goto-prev-error))
 
-;;;; Org
-(with-eval-after-load 'org
-  (define-key org-mode-map (kbd "C-M-<return>") #'org-insert-subheading))
+;;; Global modes
+(column-number-mode 1)
+(save-place-mode 1)
+(global-so-long-mode 1)
+(winner-mode 1)
+(minibuffer-depth-indicate-mode 1)
+(savehist-mode 1)
+
+(setq repeat-exit-key "RET")
+(repeat-mode 1)
+
+(add-to-list 'load-path (locate-user-emacs-file "lisp"))
+(require 'my-display-buffer)
 
 ;;; Custom
 (setq custom-file (locate-user-emacs-file "custom.el"))
 (load custom-file t)
-
-;;; Local
-;; Local Variables:
-;; outline-default-state: outline-show-only-headings
-;; End:
---
+++ lisp/my-display-buffer.el
@@ -0,0 +1,74 @@
+;;; my-display-buffer.el ---                         -*- lexical-binding: t; -*-
+(defun fit-window-to-buffer-max-fifth-frame (&optional window)
+  "Fit WINDOW to buffer size, but max height is 20% of the frame height."
+  (interactive)
+  (let ((window (or window (selected-window)))
+        (max-height (/ (frame-height) 5)))
+    (fit-window-to-buffer window max-height)))
+
+(defun display-completion-buffer (buffer actions)
+  "Display completion buffer."
+  (if (minibuffer-window-active-p (selected-window))
+      (display-buffer-in-direction buffer actions)
+    (display-buffer-below-selected buffer actions)))
+
+
+(setq switch-to-buffer-obey-display-actions t)
+(setq display-buffer-alist
+      '(("\\*Completions\\*"
+	 (display-completion-buffer)
+	 (dedicated . t)
+	 (direction . down)
+	 (window . root)
+	 (window-parameters . ((mode-line-format . (" " mode-line-buffer-identification))))
+	 (window-height . fit-window-to-buffer-max-fifth-frame))
+
+	("\\*Flymake\\|Buffer List*"
+	 display-buffer-in-side-window
+         (side . bottom)
+	 (slot . -1)
+	 (window-height . 15)
+	 (dedicated . t)
+	 (preserve-size . (t . nil))
+	 (body-function . select-window)
+	 (window-parameters . ((no-other-window . t)
+			       (no-delete-other-windows . t)
+			       (mode-line-format . (" " mode-line-buffer-identification)))))
+
+	("\\*Messages\\*"
+	  display-buffer-in-side-window
+	 (side . bottom)
+	 (slot . 1)
+	 (dedicated . t)
+	 (preserve-size . (t . nil))
+	 (window-height . 15)
+	 (body-function . select-window)
+	 (window-parameters . ((no-other-window . t)
+			       (no-delete-other-windows . t)
+			       (mode-line-format . (" " mode-line-buffer-identification)))))
+
+	("\\*\\(Occur\\|grep\\)\\**"
+	 display-buffer-in-side-window
+	 (side . bottom)
+	 (slot . 1)
+	 (dedicated . t)
+	 (preserve-size . (t . nil))
+	 (window-height . 15)
+	 (body-function . select-window)
+	 (window-parameters . ((mode-line-format . (" ")))))
+
+	("\\*eldoc*"
+	 (display-buffer-reuse-window
+	  display-buffer-in-side-window)
+	 (reusable-frames . nil)
+	 (side . right)
+	 (slot . 1)
+	 (dedicated . t)
+	 (preserve-size . (t . nil))
+	 (window-width . 30)
+	 (body-function . select-window)
+	 (window-parameters . ((no-other-window . t)
+			       (mode-line-format . ("" mode-line-buffer-identification)))))))
+
+(provide 'my-display-buffer)
+;;; my-display-buffer.el ends here
