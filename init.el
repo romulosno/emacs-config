bcbf535024d1d2f3f4a23424326f43dccf089514
Romulo Souza Nascimento Oliveira <romulo@rome>
2025-05-08 13:07:29 -0300

    Reorder and refactoring

--- init.el
+++ init.el
@@ -19,9 +19,9 @@
 (setq auto-dark-themes '((rom-night) (rom-day)))
 (auto-dark-mode 1)
 
-(setq backup-by-copying t)
-(setq delete-old-versions t)
-(setq version-control t)
+(setq backup-by-copying t
+      delete-old-versions t
+      version-control t)
 
 (setq backup-directory-alist
       `((,tramp-file-name-regexp . ,temporary-file-directory)
@@ -58,70 +58,86 @@
 (setq read-minibuffer-restore-windows nil)
 (setq sentence-end-double-space nil)
 (setq uniquify-buffer-name-style 'forward)
-(setq find-file-visit-truename t)
+(setq delete-by-moving-to-trash t)
+
+(setq find-file-visit-truename t
+      vc-follow-symlinks t)
+
 (setq repeat-exit-key "RET")
+(repeat-mode 1)
 
 (put 'narrow-to-region 'disabled nil)
 
 (setq eval-expression-print-level nil)
 (setq eval-expression-print-length nil)
 
-(setq isearch-lazy-count t)
-(setq isearch-allow-scroll t)
-(setq isearch-yank-on-move 'shift)
-(setq isearch-lazy-highlight 'all-windows)
+(setq isearch-lazy-count t
+      isearch-allow-scroll t
+      isearch-yank-on-move 'shift
+      isearch-lazy-highlight 'all-windows)
 
-(setq imenu-flatten 'prefix)
-(setq imenu-auto-rescan t)
+(setq imenu-flatten 'prefix
+      imenu-auto-rescan t)
 
-(setq tab-always-indent 'complete)
-(setq completion-show-help nil)
-(setq completions-header-format nil)
-(setq completions-max-height 20)
-(setq completions-format 'one-column)
-(setq completion-styles '(basic partial-completion initials substring))
+(setq tab-always-indent 'complete
+      completion-show-help nil
+      completions-header-format nil
+      completions-max-height 20
+      completions-format 'one-column
+      completion-styles '(basic partial-completion initials substring))
+
+(global-completion-preview-mode 1)
 
 (setq-default abbrev-mode t)
 (setq dabbrev-case-fold-search nil)
-(setq save-abbrevs 'silently)
-(setq abbrev-file-name (expand-file-name "abbrev_defs" user-emacs-directory))
+
+(setq save-abbrevs 'silently
+      abbrev-file-name (expand-file-name "abbrev_defs" user-emacs-directory))
+
+(setq savehist-additional-variables
+      '(kill-ring
+	register-alist
+	mark-ring
+	global-mark-ring
+	search-ring regexp-search-ring))
+
+(savehist-mode 1)
 
 (require 'dired-aux)
 (require 'dired-x)
-(require 'ls-lisp)
 
-(setq ls-lisp-dirs-first t)
-(setq ls-lisp-use-insert-directory-program nil)
+(setq dired-kill-when-opening-new-dired-buffer t
+      dired-recursive-copies 'always
+      dired-recursive-deletes 'always)
 
-(setq delete-by-moving-to-trash t)
-(setq dired-kill-when-opening-new-dired-buffer t)
-(setq dired-recursive-copies 'always)
-(setq dired-recursive-deletes 'always)
+(setq ls-lisp-dirs-first t
+      ls-lisp-use-insert-directory-program nil)
 
-(setq org-agenda-show-log t)
-(setq org-export-with-sub-superscripts nil)
-(setq org-goto-interface 'outline-path-completion)
-(setq org-goto-max-level 10)
-(setq org-log-done 'time)
-(setq org-log-into-drawer t)
-(setq org-return-follows-link t)
-(setq org-startup-folded nil)
-(setq org-startup-indented t)
-(setq org-tags-column 0)
-(setq org-use-speed-commands t)
+(setq org-agenda-show-log t
+      org-log-done 'time
+      org-log-into-drawer t)
+
+(setq org-goto-interface 'outline-path-completion
+      org-goto-max-level 10)
+
+(setq org-export-with-sub-superscripts nil
+      org-return-follows-link t
+      org-startup-folded nil
+      org-startup-indented t
+      org-tags-column 0
+      org-use-speed-commands t)
 
 (put 'org-todo-keyword-faces 'safe-local-variable #'stringp)
 
 (add-hook 'latex-mode-hook #'visual-line-mode)
 (add-hook 'latex-mode-hook #'electric-pair-mode)
 
-(setq vc-git-show-stash 0)
-(setq vc-follow-symlinks t)
-(setq vc-git-print-log-follow t)
-(setq vc-handled-backends '(Git))
+(setq vc-git-show-stash 0
+      vc-git-print-log-follow t
+      vc-handled-backends '(Git))
 
-(setq ediff-window-setup-function 'ediff-setup-windows-plain)
-(setq ediff-keep-variants nil)
+(setq ediff-window-setup-function 'ediff-setup-windows-plain
+      ediff-keep-variants nil)
 
 (setq smerge-command-prefix "\e")
 (setq diff-default-read-only t)
@@ -129,28 +145,26 @@
 (add-hook 'prog-mode-hook #'electric-pair-local-mode)
 (add-hook 'compilation-filter-hook #'ansi-color-compilation-filter)
 
-(setq compilation-max-output-line-length nil)
-(setq compilation-scroll-output 'first-error)
-(setq compilation-auto-jump-to-first-error 'if-location-known)
-(setq compilation-ask-about-save nil)
-
-(setq eldoc-documentation-strategy 'eldoc-documentation-compose-eagerly)
-(setq eldoc-echo-area-use-multiline-p 2)
-
 (setq read-process-output-max (* 3 1024 1024))
-(setq eglot-autoshutdown t)
-(setq eglot-sync-connect nil)
-(setq eglot-events-buffer-config '(:size 0))
+
+(setq compilation-ask-about-save nil
+      compilation-max-output-line-length nil
+      compilation-scroll-output 'first-error
+      compilation-auto-jump-to-first-error 'if-location-known)
+
+(setq eldoc-documentation-strategy 'eldoc-documentation-compose-eagerly
+      eldoc-echo-area-use-multiline-p 2)
+
+(setq eglot-autoshutdown t
+      eglot-sync-connect nil
+      eglot-events-buffer-config '(:size 0))
       
 (add-to-list 'display-buffer-alist '("\\*Buffer List\\*" nil (body-function . select-window)))
 
 (column-number-mode 1)
 (global-auto-revert-mode 1)
-(global-completion-preview-mode 1)
 (global-so-long-mode 1)
-(repeat-mode 1)
 (save-place-mode 1)
-(savehist-mode 1)
 (winner-mode 1)
 
 (keymap-set global-map "<remap> <count-words-region>" #'count-words)
