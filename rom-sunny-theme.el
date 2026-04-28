e3c07dcb4a450ca668d304382a398c8dd5b3aa18
romulo <romulo@souza>
2026-04-28 15:28:33 -0300

    Up

---
+++ rom-sunny-theme.el
@@ -0,0 +1,62 @@
+;;; rom-sunny-theme.el --- rom-sunny
+
+(deftheme rom-sunny
+  "Light theme."
+  :kind 'color-scheme
+  :family 'rom
+  :background-mode 'light)
+
+(custom-theme-set-faces
+ 'rom-sunny
+ '(cursor ((t (:background "#cf8300"))))
+ '(completions-common-part ((t (:foreground "#4f4941"))))
+ '(default ((t (:background "#f2efdc" :foreground "#000000"))))
+ '(diff-added ((t (:background "#daebda"))))
+ '(diff-refine-added ((t (:background "#bce2be"))))
+ '(diff-refine-removed ((t (:background "#f1caca"))))
+ '(diff-removed ((t (:background "#f1dede"))))
+ '(error ((t (:foreground "#961818"))))
+ '(eshell-prompt ((t (:foreground "#008b8b"))))
+ '(flymake-error ((t (:underline "#961818"))))
+ '(flymake-note ((t (:underline "#1c6625"))))
+ '(flymake-warning ((t (:underline "#805801"))))
+ '(font-lock-builtin-face ((t (:foreground "#08545c"))))
+ '(font-lock-comment-face ((t (:foreground "#5c5845"))))
+ '(font-lock-constant-face ((t (:foreground "#284d2b"))))
+ '(font-lock-function-name-face ((t (:foreground "#4a005c"))))
+ '(font-lock-keyword-face ((t (:foreground "#55166e"))))
+ '(font-lock-string-face ((t (:foreground "#073869"))))
+ '(font-lock-type-face ((t (:foreground "#191970"))))
+ '(font-lock-variable-name-face ((t (:foreground "#1d3759"))))
+ '(font-lock-warning-face ((t (:foreground "#ff0000" :bold t))))
+ '(fringe ((t (:background "#f2eddc"))))
+ '(highlight ((t (:bold t))))
+ '(isearch ((t (:foreground "#b0e2ff" :background "#4682b4"))))
+ '(minibuffer-prompt ((t (:foreground "#171f34"))))
+ '(mode-line ((t (:foreground "#000000" :background "#b7c3c4" :box (:color "#a2b6ba")))))
+ '(mode-line-inactive ((t (:foreground "#757575" :background "#c9d3d4" :box (:color "#a2b6ba")))))
+ '(org-date ((t (:foreground "#3b3b3b" :underline t))))
+ '(org-done ((t (:foreground "#6b8e23" :weight bold))))
+ '(org-headline-done ((t (:foreground "#042b75"))))
+ '(outline-1 ((t (:extend nil :foreground "#02112f" :bold t))))
+ '(outline-2 ((t (:extend nil :foreground "#02163b" :bold t))))
+ '(outline-3 ((t (:extend nil :foreground "#031e52" :bold t))))
+ '(outline-4 ((t (:extend nil :foreground "#03225e" :bold t))))
+ '(outline-5 ((t (:extend nil :foreground "#042769" :bold t))))
+ '(outline-6 ((t (:extend nil :foreground "#042b75" :bold t))))
+ '(outline-7 ((t (:extend nil :foreground "#1d4083" :bold t))))
+ '(outline-8 ((t (:extend nil :foreground "#365591" :bold t))))
+ '(org-todo ((t (:foreground "#8b0000" :weight bold))))
+ '(pulse-highlight-face ((t (:background "#8eb4d1" :extend nil))))
+ '(pulse-highlight-start-face ((t (:background "#8eb4d1"))))
+ '(region ((t (:background "#cccccc"))))
+ '(secondary-selection ((t (:background "#cad8db"))))
+ '(success ((t :foreground "#1c6625")))
+ '(tab-bar ((t (:foreground "#000000" :background "#b7c3c4" :box (:color "#a2b6ba")))))
+ '(tab-bar-tab ((t (:foreground "#000000" :background "#b7c3c4" :box (:color "#a2b6ba")))))
+ '(tab-bar-tab-inactive ((t (:inherit tab-bar :foreground "#959292"))))
+ '(warning ((t (:foreground "#805801"))))
+ '(whitespace-space ((t (:foreground "#a8a8a8")))))
+
+(provide-theme 'rom-sunny)
+;;; rom-sunny-theme.el ends here
